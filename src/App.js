f75734253d676551b62724f790476ad351ee6b64
charliton254 <55441672+charliton254@users.noreply.github.com>
2022-09-10 16:31:15 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
 import Home from './pages/Home';
 import Cuisine from './pages/Cuisine';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Icons from './components/Icons';
+import Search from './components/Search';
+import Searched from './pages/Searched';
+import Recipe from './pages/Recipe';
+import Nav from './components/Nav';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
-      <Routes>
-        <Route path='/Cuisine/:type' element={ <Cuisine/> } />
+      <Nav/>
+      <Search/>
+      <Icons/>
+      <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
         <Route path='/' element={ <Home/> } />
+        <Route path='/Cuisine/:type' element={ <Cuisine/> } />
+        <Route path='/searched/:search' element={ <Searched/> } />
+        <Route path='/recipe/:name' element={<Recipe/>} />
       </Routes>
+      </AnimatePresence>
     </div>
   );
 }
